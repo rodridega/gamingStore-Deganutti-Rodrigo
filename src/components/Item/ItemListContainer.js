35b5c5ab07b418062ee5941e52f3997e2fd6218e
rodridega <56453875+rodridega@users.noreply.github.com>
2022-03-08 22:00:07 -0300

    Add ItemCount

--- src/components/Item/ItemListContainer.js
+++ src/components/Item/ItemListContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ItemCount } from '../ItemCount/ItemCount';
 import './ItemListContainer.css'
 
 const ItemListContainer = ({greeting}) => {
     return ( 
+        <>
         <h2 className='fs-1 m-4 text-center text-white'>{greeting}</h2>
+        <ItemCount/>
+        </>
      );
 }
  
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Card, Button, Row, Col } from "react-bootstrap";
+import "./ItemCount.css";
+
+export const ItemCount = () => {
+  const [counter, setCounter] = useState(0);
+  const [stock, setStock] = useState(5);
+
+  const onMinus = () => {
+    if (counter <= 0) {
+      return;
+    } else {
+      setCounter(counter - 1);
+      setStock(stock + 1);
+    }
+  };
+  const onAdd = () => {
+    if (stock <= 0) {
+      return;
+    } else {
+      setCounter(counter + 1);
+      setStock(stock - 1);
+    }
+  };
+
+  return (
+    <Card className="mx-auto" style={{ width: "18rem" }}>
+      <Card.Img
+        variant="top"
+        src="https://media.vandal.net/m/79072/world-of-warcraft-shadowlands-20209291145071_1.jpg"
+      />
+      <Card.Body className="text-white bg-secondary border-none">
+        <Card.Title> World of Warcraft</Card.Title>
+        <Card.Text>
+          WoW Shadowlands - Enfrentate al mas allá... El velo entre la vida y la
+          muerte ya no existe. WoW Shadowlands
+        </Card.Text>
+        <Card.Text>Stock: {stock}</Card.Text>
+        <Row className="align-items-center justify-content-center">
+          <Col xs="auto">
+            <Button className="mb-2 px-3 py-1" variant="dark" onClick={onMinus}>
+              -
+            </Button>
+          </Col>
+          <Col xs="auto">
+            {" "}
+            <div className="border rounded mb-2 px-3 py-1 fs-4">{counter}</div>
+          </Col>
+          <Col xs="auto">
+            {" "}
+            <Button className="mb-2 px-3 py-1" variant="dark" onClick={onAdd}>
+              +
+            </Button>
+          </Col>
+        </Row>
+        <div className="d-grid gap-2">
+          <Button variant="dark" size={"md"}>
+            Agregar
+          </Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
